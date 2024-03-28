1941e2702a1391cc2aae98067d489af65c7d8a96
AhmetAtinc <ahmetatnc@gmail.com>
2024-03-28 16:20:06 +0300

    editedServiceWarehouseAppsName

---
+++ service_warehouse/service_marketplace/doctype/service_packet_subscription/service_packet_subscription.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, a-techsyn and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ServicePacketSubscription(Document):
+	pass
---
+++ service_warehouse/service_marketplace/doctype/service_packet_version/service_packet_version.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, a-techsyn and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Service Packet Version", {
+// 	refresh(frm) {
+
+// 	},
+// });
