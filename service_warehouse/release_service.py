104f1870f227adacc045e8b20342f07ddc048557
S&P <spehlivan38@gmail.com>
2024-04-21 05:47:56 +0300

    minor updates on release service

--- service_warehouse/release_service.py
+++ service_warehouse/release_service.py
@@ -3,12 +3,11 @@ import frappe
 
 
 @frappe.whitelist()
-def check_update(*args, **kwargs):
-	tenant_code = kwargs.get('tenant_code')
-
+def check_update(tenant_code):
+	
 	# get service subscription filter by tenant
 	service_packets = frappe.get_all("Service Subscription", fields=["service_packet"],
-									 filters={"tenant_code": tenant_code})
+									 filters={"tenant": tenant_code})
 
 	# get service package filter by service_packets
 	service_packages = frappe.get_all("Service Packet", fields=["*"],
@@ -19,7 +18,7 @@ def check_update(*args, **kwargs):
 		package = {
 			"name": service_package.name,
 			"title": service_package.title,
-			"latest_release": service_package.latest_release,
+			"release_version": service_package.latest_release,
 			"service_provider": service_package.service_provider
 		}
 		packages.append(package)
@@ -27,16 +26,22 @@ def check_update(*args, **kwargs):
 
 
 @frappe.whitelist()
-def get_service_package(*args, **kwargs):
-	input = json.loads(kwargs.get('input'))
+def get_service_package(packet_release_version):
+	
+	packet_name = "_".join(packet_release_version.split("_")[:-1])
 
-	service_package = frappe.get_doc("Service Packet", "_".join(input.get('name').split("_")[:-1]))
-	service_package_version = frappe.get_doc("Service Packet Version", input.get('name'))
+	service_package = frappe.get_doc("Service Packet", packet_name)
+	service_package_version = frappe.get_doc("Service Packet Version", packet_release_version)
+	packet_service_provider = frappe.get_doc("Service Provider", service_package.get('service_provider'))
 	extensions = service_package_version.get("extensions")
 	extension_docs = []
+	extension_type_cache = {}
 	for extension in extensions:
 		extension_doc = frappe.get_doc("Service Extension",
 									   "_".join(extension.get('name').split("_")[:-1]))
+		if extension_doc.extension_type not in extension_type_cache:
+			extension_type_cache[extension_doc.extension_type] = frappe.get_doc("Service Extension Type", extension_doc.extension_type, fields=["name", "title"])
+		extension_type_doc = extension_type_cache[extension_doc.extension_type]
 		extension_model = {
 			"title": extension_doc.title,
 			"library_name": extension_doc.library_name,
@@ -45,7 +50,7 @@ def get_service_package(*args, **kwargs):
 			"minor": extension_doc.minor,
 			"extension_type": {
 				"name": extension_doc.extension_type,
-				"title": "extension_doc.extension_type_title"
+				"title": extension_type_doc.title
 			},
 		}
 		extension_docs.append(extension_model)
@@ -57,9 +62,8 @@ def get_service_package(*args, **kwargs):
 		'minor': service_package_version.get('minor'),
 		'service_provider': {
 			'name': service_package.get('service_provider'),
-			'title': 'Provider 1'
+			'title': packet_service_provider.title
 		},
 		'extensions': extension_docs
 	}
-	print("input: ", input)
 
