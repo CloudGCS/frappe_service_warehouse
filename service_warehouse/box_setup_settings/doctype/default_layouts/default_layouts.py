d56b186cda19b8d14ced45b6330e62c9efd5543d
Engin Can Altınöz <76838257+enginc4n@users.noreply.github.com>
2024-12-24 21:49:29 +0300

    CLOUD-703 (#2)
    box setup configuration: default layouts

---
+++ service_warehouse/box_setup_settings/doctype/default_layouts/default_layouts.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2024, CloudGCS and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class DefaultLayouts(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		layout: DF.JSON
+		platform: DF.Literal["Desktop", "Mobile"]
+		source: DF.Literal["Default", "Tenant"]
+		viewer: DF.Literal["Pilot", "Observer", "Test"]
+	# end: auto-generated types
+	pass
+
+@frappe.whitelist()
+def get_default_layouts():
+	default_layouts = frappe.get_all("Default Layouts",fields=["name","platform","viewer","source","layout"] ,filters={"source": "Default"})
+	return default_layouts
---
+++ service_warehouse/box_setup_settings/doctype/default_layouts/test_default_layouts.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, CloudGCS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDefaultLayouts(FrappeTestCase):
+	pass
